a76b4b39ac80f93c223ddaa2e92ba7949c2a6b6f
peder280370 <peder@carolus.dk>
2015-02-06 12:14:12 +0100

    Added Docker

---
+++ docker/start.sh
@@ -0,0 +1,12 @@
+
+if [ -z "${AIS_VIEW_URL}" ]; then
+	AIS_VIEW_URLP=""
+
+else
+	AIS_VIEW_URLP="https://ais2.e-navigation.net/aisview/rest/store/query?"
+fi
+
+
+LATEST=`ls /archive/target/ais-downloader*SNAPSHOT.war`
+
+java -jar $LATEST $AIS_VIEW_URLP
